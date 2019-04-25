787e9c4e95f2d405f88a3b8debbebecbd881ad5e
Chi Thanh Nguyen <c.nguyen@nchain.com>
2019-04-25 10:11:22 +0100

    add documentation how to build Openssl WASM

--- cmake/modules/FindOpenSSLHelper.cmake
+++ cmake/modules/FindOpenSSLHelper.cmake
@@ -10,12 +10,44 @@ if(FindOpenSSLHelper_Include)
 endif()
 set(FindOpenSSLHelper_Include TRUE)
 
-#### For Windows, download binary prebuilt OpenSSL and set %OPENSSL_ROOT_DIR% pointing to the root of the installed directory
-#### Take the first suggestion from https://wiki.openssl.org/index.php/Binaries. splroweb
-#### For Linux, build from source using the command :
-####     ./config shared --prefix=/path/to/installed/directory --openssldir=/path/to/installed/directory && make depend && make -j8 && make test && make install
-#### set $OPENSSL_ROOT_DIR pointing to the root of the installed directory
+## For Windows, download binary prebuilt OpenSSL and set %OPENSSL_ROOT_DIR% pointing to the root of the installed directory
+## Take the first suggestion from https://wiki.openssl.org/index.php/Binaries. splroweb
+## For Linux, build from source using the command :
+##     ./config shared --prefix=/path/to/installed/directory --openssldir=/path/to/installed/directory && make depend && make -j8 && make test && make install
+## set $OPENSSL_ROOT_DIR pointing to the root of the installed directory
+##
+## 
+## 
+#[[############################ Build openssl wasm  ############################
+# Emscripten build is really portable cross plateform, so it only need to build on linux (easier) then the wasm build can be used on windows (for wasm only)
+# Download and install emsdk following emscripten documentation https://emscripten.org/docs/getting_started/downloads.html
+# set environment variables in ~/.bashrc
+#export EMSDK_ROOT=/path/to/where/emsdk/installed
+#export OPENSSL_WASM_ROOT_DIR=/path/to/where/openssl-1.1.1b_wasm/will/be/installed
 
+source ~/.bashrc
+wget https://www.openssl.org/source/openssl-1.1.1b.tar.gz  # Download openssl-1.1.1b
+tar xvzf openssl-1.1.1b.tar.gz                             # Extract it
+mv openssl-1.1.1b openssl-1.1.1b_src                       # Rename openssl source directory
+cd openssl-1.1.1b_src                                      # Goto openssl source directory
+source $EMSDK_ROOT/emsdk_env.sh                            # Setup emscripten environment variables
+## Configure the build for openssl
+emconfigure ./Configure --openssldir=/path/to/some/tmp/build/dir --prefix=$OPENSSL_WASM_ROOT_DIR -no-shared linux-x86_64
+
+## Fix some minor issues : https://github.com/openssl/openssl/issues/5443
+# Go to Makefile, make those changes :
+# - CROSS_COMPILE set to blank
+# - CNF_CFLAGS remove any of the options -arch armv7 -mios-version-min=6.0.0 -fno-common -isysroot # for openssl-1.1.b, it's ok to not doing anything
+# - CFLAGS add -D__STDC_NO_ATOMICS__=1
+
+emmake make ; emmake make install                          # Build and install openssl wasm
+## To test the linking, create some test file using openssl, then
+cd /path/to/some/tmp/directory/
+emcc test_link_openssl_wasm.c -lssl -lcrypto -L $OPENSSL_WASM_ROOT_DIR/lib -I $OPENSSL_WASM_ROOT_DIR/include -s WASM=1 -o index.html
+python3 -m http.server
+## open your browser and open https://locallhost.com/
+############################ Build openssl wasm  ############################
+]]
 
 #### Preset some variable to find and import OpenSSL
 function(presetOpenSSLVariable)##########################################################################
