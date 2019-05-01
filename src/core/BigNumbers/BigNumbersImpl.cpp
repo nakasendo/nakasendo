a01e4e82ca6e6fbd2f0e473100001874b5fe9aee
Chi Thanh NGUYEN <c.nguyen@nchain.com>
2019-05-01 16:14:33 +0100

    replace std::cout printing by throwing exception in src/core

--- src/core/BigNumbers/BigNumbersImpl.cpp
+++ src/core/BigNumbers/BigNumbersImpl.cpp
@@ -6,18 +6,16 @@
 
 std::unique_ptr<BigNumberImpl> Add (const BigNumberImpl* obj1, const BigNumberImpl* obj2){
     BN_ptr res(BN_new(), ::BN_free);
-    if (!BN_add(res.get(),obj1->m_bn.get(),obj2->m_bn.get())){
-        std::cout << "error" << std::endl; 
-    }
+    if (!BN_add(res.get(),obj1->m_bn.get(),obj2->m_bn.get()))
+        throw std::runtime_error("error");
     std::unique_ptr<BigNumberImpl> ResImpl (new BigNumberImpl (res) ) ; 
     return std::move(ResImpl);
 }
 
 std::unique_ptr<BigNumberImpl> Sub (const BigNumberImpl* obj1, const BigNumberImpl* obj2){
     BN_ptr res (BN_new(),::BN_free);
-    if(!BN_sub(res.get(), obj1->m_bn.get(), obj2->m_bn.get())){
-        std::cout << "error" << std::endl ; 
-    }
+    if (!BN_sub(res.get(), obj1->m_bn.get(), obj2->m_bn.get()))
+        throw std::runtime_error("error");
     std::unique_ptr<BigNumberImpl> ResImpl (new BigNumberImpl(res));
     return std::move(ResImpl);
 }
@@ -27,9 +25,7 @@ std::unique_ptr<BigNumberImpl> Mod (const BigNumberImpl* obj1, const BigNumberIm
     BN_CTX* ctxptr = BN_CTX_new(); 
     BN_ptr res (BN_new(),::BN_free);
     if (!BN_mod(res.get(), obj1->m_bn.get(), obj2->m_bn.get(), ctxptr))
-    {
-        std::cout << "Error" << std::endl ; 
-    }
+        throw std::runtime_error("error");
     std::unique_ptr<BigNumberImpl> ResImpl(new BigNumberImpl(res));
     BN_CTX_free(ctxptr);
     return std::move(ResImpl);
@@ -39,9 +35,8 @@ std::unique_ptr<BigNumberImpl> Mul (const BigNumberImpl* obj1, const BigNumberIm
 {
     BN_CTX* ctxptr = BN_CTX_new(); 
     BN_ptr res (BN_new(),::BN_free);
-    if (!BN_mul(res.get(), obj1->m_bn.get(), obj2->m_bn.get(), ctxptr)){
-        std::cout << "error" << std::endl; 
-    }
+    if (!BN_mul(res.get(), obj1->m_bn.get(), obj2->m_bn.get(), ctxptr))
+        throw std::runtime_error("error");
     std::unique_ptr<BigNumberImpl> ResImpl (new BigNumberImpl (res) ) ; 
     return std::move(ResImpl);
 }
@@ -50,9 +45,8 @@ std::unique_ptr<BigNumberImpl> Div (const BigNumberImpl* obj1, const BigNumberIm
 {
     BN_CTX* ctxptr = BN_CTX_new(); 
     BN_ptr res (BN_new(),::BN_free);
-    if (!BN_div(res.get(), NULL, obj1->m_bn.get(), obj2->m_bn.get(), ctxptr)){
-        std::cout << "error" << std::endl; 
-    }
+    if (!BN_div(res.get(), NULL, obj1->m_bn.get(), obj2->m_bn.get(), ctxptr))
+        throw std::runtime_error("error");
     std::unique_ptr<BigNumberImpl> ResImpl (new BigNumberImpl (res) ) ; 
     return std::move(ResImpl);
 
@@ -63,9 +57,7 @@ std::unique_ptr<BigNumberImpl>  LShift(const BigNumberImpl* obj, const int& val)
     BN_ptr res(BN_new(), ::BN_free);
 
     if(!BN_lshift(res.get(), obj->m_bn.get(), val))
-    {
-        std::cout << "Unable to left-shift by " << val << std::endl ; 
-    }
+        throw std::runtime_error("Unable to left-shift by " + std::to_string(val));
     std::unique_ptr<BigNumberImpl> ResImpl (new BigNumberImpl (res) ) ;
     return std::move(ResImpl);
 }
@@ -75,9 +67,7 @@ std::unique_ptr<BigNumberImpl> RShift(const BigNumberImpl* obj, const int& val)
     BN_ptr res(BN_new(), ::BN_free);
 
     if(!BN_rshift(res.get(), obj->m_bn.get(), val))
-    {
-        std::cout << "Unable to right-shift by " << val << std::endl ; 
-    }
+        throw std::runtime_error("Unable to right-shift by " + std::to_string(val));
     std::unique_ptr<BigNumberImpl> ResImpl (new BigNumberImpl (res) ) ;
     return std::move(ResImpl);
 }
@@ -128,9 +118,8 @@ BigNumberImpl& BigNumberImpl::operator++()
 {
     BN_ptr res (BN_new(),::BN_free);
     BN_one(res.get());
-    if(!BN_add(m_bn.get(),m_bn.get(), res.get())){
-        std::cout << "Unable to increment by one" << std::endl ; 
-    }
+    if(!BN_add(m_bn.get(),m_bn.get(), res.get()))
+        throw std::runtime_error("Unable to increment by one");
     return *this ; 
 }
 
@@ -146,9 +135,8 @@ BigNumberImpl& BigNumberImpl::operator--()
 {
     BN_ptr res (BN_new(),::BN_free);
     BN_one(res.get());
-    if(!BN_sub(m_bn.get(),m_bn.get(), res.get())){
-        std::cout << "Unable to decrement by one" << std::endl ; 
-    }
+    if(!BN_sub(m_bn.get(),m_bn.get(), res.get()))
+        throw std::runtime_error("Unable to decrement by one");
     return *this ; 
 }
 
@@ -198,9 +186,7 @@ void BigNumberImpl::generate(const int& nsize)
 {
     // todo seed the PRNG
     if ( !BN_rand(m_bn.get(), nsize, 0,0))
-    {
-        std::cout << "error generating random number" << std::endl ; 
-    }
+        throw std::runtime_error("error generating random number");
 }
 
 void BigNumberImpl::generateNeg (const int& nsize)
@@ -213,9 +199,7 @@ void BigNumberImpl::generateNeg (const int& nsize)
  {
      // seed the PRNG
     if ( !BN_rand_range(m_bn.get(),max->m_bn.get()))
-    {
-        std::cout << "error generating random number in range 0 - " << max->ToHex ()  << std::endl ; 
-    }      
+        throw std::range_error("error generating random number in range 0 - " + max->ToHex());
     return ; 
  }
 
