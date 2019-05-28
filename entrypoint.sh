c219d911d0ab756161b115b7a7a2a0c1fe3edf0f
Peter Foster <p.foster@nchain.com>
2019-05-28 12:31:11 +0000

    Modified now to build both release and debug versions and to compile with -j8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-ls -alrt
-pwd
-mkdir -p build
-cd build
-cmake ..
-make
+mkdir -p releasebuild
+cd releasebuild
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCUSTOM_SYSTEM_OS_NAME=Ubuntu
+make -j8
+cd -
+
+mkdir -p debugbuild
+cd debugbuild
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCUSTOM_SYSTEM_OS_NAME=Ubuntu
+make -j8
