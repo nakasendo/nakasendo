6f31baeaa0539ee127376b62c32f250b3acf47f1
Peter Foster <p.foster@nchain.com>
2019-06-14 06:47:42 +0000

    made -j switch dynamically read the number of cores

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 mkdir -p releasebuild
 cd releasebuild
 cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCUSTOM_SYSTEM_OS_NAME=Ubuntu
-make -j8
+make -j$(nproc --all)
 cd -
 
 mkdir -p debugbuild
 cd debugbuild
 cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCUSTOM_SYSTEM_OS_NAME=Ubuntu
-make -j8
+make -j$(nproc --all)
