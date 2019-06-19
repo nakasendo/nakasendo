5b393eb21f0aa827302b0c9a2f412e97b06202eb
davies <davies@ncrypt.com>
2019-06-19 16:12:45 +0100

    SL-104 Create test scripts for Symmetric Encryption/Decryption AES python interfaces

--- tests/functional/test_AsymKey_Test.py
+++ tests/functional/test_AsymKey_Test.py
@@ -4,6 +4,7 @@ def test_GenerateKeyPairHex():
     for x in range(100):
         # Generate pair of keys in hex format
         privKey, pubKey = PyAsymKey.GenerateKeyPairHEX()
+
         # Verifying the the length of actual value as 66
         assert len(privKey) == 66, "Test failed"
 
@@ -12,6 +13,7 @@ def test_GenerateKeyPairPEM():
     for x in range(100):
         # Generate pair of keys in pem format
         pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
+
         # Verifying the the length of actual value as 66
         assert privKeyPEMHeader in privKey, "Test failed"
 
@@ -21,8 +23,9 @@ def test_GetKeyPairHEX():
         # Generate pair of keys in pem format
         pubKeyPEM, privKeyPEM = PyAsymKey.GenerateKeyPairPEM()
         pubKeyHEX, privKeyHEX = PyAsymKey.GetKeyPairHEX(privKeyPEM)
+
         # Keys hex are generated
-        assert len(pubKeyHEX)>0, "Test failed"
+        assert len(pubKeyHEX) > 0, "Test failed"
         assert len(privKeyHEX) > 0, "Test failed"
 
 def test_GetPublicKeyPEM():
@@ -31,6 +34,7 @@ def test_GetPublicKeyPEM():
         # Generate pair of keys in pem format
         pubKeyPEM, privKeyPEM = PyAsymKey.GenerateKeyPairPEM()
         test_pubKeyPEM = PyAsymKey.GetPublicKeyPEM(privKeyPEM)
+
         # Calculated public key should match the generated one
         assert pubKeyPEM==test_pubKeyPEM, "Test failed"
 
@@ -40,10 +44,13 @@ def test_Sign_Verification():
     for x in range(100):
         # Generate pair of keys in PEM format
         pubKey, priKey = PyAsymKey.GenerateKeyPairPEM()
-        # Sign the message
+
+        # Sign message with private Key
         rSig, sSig = PyAsymKey.Sign(msg, priKey)
-        # Verify the Signature
+
+        # Verify message's signature with public key
         verify_ok = PyAsymKey.Verify(msg, pubKey, rSig, sSig)
+
         assert verify_ok, "Test failed"
 
 def test_ShareSecret():
@@ -52,8 +59,11 @@ def test_ShareSecret():
         # Generate keys for alice and bob
         alice_pubKeyPEM, alice_privKeyPEM = PyAsymKey.GenerateKeyPairPEM()
         bob_pubKeyPEM, bob_privKeyPEM = PyAsymKey.GenerateKeyPairPEM()
+
+        #Calculate shared secret from my private key and their public key
         secret_share_from_alice = PyAsymKey.ShareSecret(alice_privKeyPEM, bob_pubKeyPEM)
         secret_share_from_bob = PyAsymKey.ShareSecret(bob_privKeyPEM, alice_pubKeyPEM)
+
         assert secret_share_from_alice==secret_share_from_bob, "Test failed"
 
 def test_KeyDerive():
@@ -64,14 +74,17 @@ def test_KeyDerive():
         alice_pubKeyPEM, alice_privKeyPEM = PyAsymKey.GenerateKeyPairPEM()
         bob_pubKeyPEM, bob_privKeyPEM = PyAsymKey.GenerateKeyPairPEM()
 
+        # Derive public key from a given public key and a additive message
         alice_derived_pub = PyAsymKey.DerivePublic(alice_pubKeyPEM, additive_msg)
         bob_derived_pub = PyAsymKey.DerivePublic(bob_pubKeyPEM, additive_msg)
 
+        # Derive pirvate key from a given private key and a additive message
         alice_derived_private = PyAsymKey.DerivePrivate(alice_privKeyPEM, additive_msg)
         bob_derived_private = PyAsymKey.DerivePrivate(bob_privKeyPEM, additive_msg)
 
+        # Get public key PEM given the private key PEM
         calc_alice_derived_pub = PyAsymKey.GetPublicKeyPEM(alice_derived_private)
         calc_bob_derived_pub = PyAsymKey.GetPublicKeyPEM(bob_derived_private)
 
-        assert calc_alice_derived_pub==alice_derived_pub, "Test failed"
+        assert calc_alice_derived_pub == alice_derived_pub, "Test failed"
         assert calc_bob_derived_pub == bob_derived_pub, "Test failed"
