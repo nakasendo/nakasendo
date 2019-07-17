78406efe411c1714d48f8c758d1ae1c4bf2e93f3
Josie Wilden <j.wilden@nchain.com>
2019-07-17 14:06:36 +0100

    SL-154 : corrected evaluate function, switched BOOST_ASSERT for BOOST_TEST and BOOST_CHECK

--- tests/unit/test_Polynomial.cpp
+++ tests/unit/test_Polynomial.cpp
@@ -15,6 +15,7 @@ BOOST_AUTO_TEST_SUITE( test_suite_Polynomial )
 
 /* Create a new polynomial using the string constructor
  *   Check the degree
+ *   Evaluate the point for x=0,1
  *   Check the coefficients
  */
 BOOST_AUTO_TEST_CASE( test_polynomial_degree1 )
@@ -31,20 +32,21 @@ BOOST_AUTO_TEST_CASE( test_polynomial_degree1 )
     fx_1.FromDec( "1" ) ;
     eval_at_0 = poly( fx_0 ) ;
     eval_at_1 = poly( fx_1 ) ;
-
-    BOOST_ASSERT  ( eval_at_0.ToDec() == "3"  ) ;
-    BOOST_ASSERT  ( eval_at_1.ToDec() == "5"  ) ;
+    
+    BOOST_TEST  ( eval_at_0.ToDec() == "3"  ) ;
+    BOOST_TEST  ( eval_at_1.ToDec() == "5"  ) ;
 
     //std::cout << "Sanity check:" << std::endl ;
-    std::cout << "eval_at_0 = " << eval_at_0.ToDec( ) << std::endl ;
-    std::cout << "eval_at_1 = " << eval_at_1.ToDec( ) << std::endl ;
+    //std::cout << "eval_at_0 = " << eval_at_0.ToDec( ) << std::endl ;
+    //std::cout << "eval_at_1 = " << eval_at_1.ToDec( ) << std::endl ;
 
-    // <todo> remove this
-    std::cout << poly << std::endl ;
+    std::ostringstream ss ;
+    ss << poly ;
+    BOOST_TEST ( ss.str( ) == "modulo = 0;	polynomial = 3 + 2x" ) ;
 }
 
-/* Create a new polynomial using the vector constructor
- *   Check the degree
+/* Create a new polynomial using the vector constr
+ *   Evaluate the point for x=0,1,2
  *   Check the coefficients
  */
 BOOST_AUTO_TEST_CASE( test_polynomial_degree2 )
@@ -62,21 +64,28 @@ BOOST_AUTO_TEST_CASE( test_polynomial_degree2 )
 
     Polynomial poly ( bnCoefficients, GenerateZero( ) ) ;
 
-    long degree = poly.getDegree( ) ;
-    BOOST_CHECK_EQUAL ( degree, strCoefficients.size() - 1 ) ;
+    BigNumber fx_0, fx_1, fx_2, eval_at_0, eval_at_1, eval_at_2;
+    fx_0.FromDec( "0" ) ;
+    fx_1.FromDec( "1" ) ;
+    fx_2.FromDec( "2" ) ;
 
-    BigNumber eval_at_0, eval_at_1, eval_at_2;
+    eval_at_0 = poly( fx_0 ) ;
+    eval_at_1 = poly( fx_1 ) ;
+    eval_at_2 = poly( fx_2 ) ;
 
-    BOOST_ASSERT  ( poly[ 0 ] == eval_at_0.FromDec( "1" )  ) ;
-    BOOST_ASSERT  ( poly[ 1 ] == eval_at_1.FromDec( "2" )  ) ;
-    BOOST_ASSERT  ( poly[ 2 ] == eval_at_2.FromDec( "3" )  ) ;
+    BOOST_TEST  ( eval_at_0.ToDec( ) == "1" ) ;
+    BOOST_TEST  ( eval_at_1.ToDec( ) == "6" ) ;
+    BOOST_TEST  ( eval_at_2.ToDec( ) == "17" ) ;
 
-    // <todo> remove this
-    std::cout << poly << std::endl ;
+    std::ostringstream ss ;
+    ss << poly ;
+    BOOST_TEST ( ss.str( ) == "modulo = 0;	polynomial = 1 + 2x + 3x^2" ) ;
 }
 
 /* Create a new polynomial using the string constructor
  *   Check the mod function
+ *   Check the [] operator
+ *   Check the f(x) for a vector
  */
 BOOST_AUTO_TEST_CASE( test_polynomial_degree2_mod )
 {
@@ -87,19 +96,38 @@ BOOST_AUTO_TEST_CASE( test_polynomial_degree2_mod )
 
     Polynomial poly ( strCoefficients, modulo ) ;
 
+    BigNumber array_0, array_1, array_2 ;
+    array_0.FromDec( "1" ) ;
+    array_1.FromDec( "2" ) ;
+    array_2.FromDec( "3" ) ;
 
-    BigNumber eval_at_0, eval_at_1, eval_at_2 ;
+    BOOST_CHECK  ( ( poly[ 0 ] ) ==  array_0 ) ;
+    BOOST_CHECK  ( ( poly[ 1 ] ) ==  array_1 ) ;
+    BOOST_CHECK  ( ( poly[ 2 ] ) ==  array_2 ) ;
 
-    BOOST_ASSERT  ( poly[ 0 ] == eval_at_0.FromDec( "1" )  ) ;
-    BOOST_ASSERT  ( poly[ 1 ] == eval_at_1.FromDec( "2" )  ) ;
-    BOOST_ASSERT  ( poly[ 2 ] == eval_at_2.FromDec( "3" )  ) ;
 
-    // <todo> remove this
-    std::cout << poly << std::endl ;
+    BigNumber fx_0, fx_1, fx_2, eval_at_0, eval_at_1, eval_at_2;
+    fx_0.FromDec( "0" ) ;
+    fx_1.FromDec( "1" ) ;
+    fx_2.FromDec( "2" ) ;
+
+    eval_at_0 = poly( fx_0 ) ;
+    eval_at_1 = poly( fx_1 ) ;
+    eval_at_2 = poly( fx_2 ) ;
+
+    BOOST_TEST  ( eval_at_0.ToDec( ) == "1" ) ;
+    BOOST_TEST  ( eval_at_1.ToDec( ) == "1" ) ;
+    BOOST_TEST  ( eval_at_2.ToDec( ) == "2" ) ;
+
+   
+    std::ostringstream ss ;
+    ss << poly ;
+    BOOST_TEST ( ss.str( ) == "modulo = 5;	polynomial = 1 + 2x + 3x^2" ) ;
 }
 
 /* Create a new polynomial using the vector constructor
  *   Check the mod function
+ *   Check the f(x) for x = 0,1,2
  */
 BOOST_AUTO_TEST_CASE( test_polynomial_degree2_mod2 )
 {
@@ -120,14 +148,32 @@ BOOST_AUTO_TEST_CASE( test_polynomial_degree2_mod2 )
     Polynomial poly ( bnCoefficients, modulo ) ;
 
 
-    BigNumber eval_at_0, eval_at_1 ;
+    BigNumber array_0, array_1, array_2;
 
-    BOOST_ASSERT  ( poly[ 0 ] == eval_at_0.FromDec( "1" )  ) ;
-    BOOST_ASSERT  ( poly[ 1 ] == eval_at_1.FromDec( "0" )  ) ;
-    BOOST_ASSERT  ( poly[ 2 ] == eval_at_2.FromDec( "4" )  ) ;
+    array_0.FromDec( "1" ) ;
+    array_1.FromDec( "0" ) ;
+    array_2.FromDec( "4" ) ;
 
-    // <todo> remove this
-    std::cout << poly << std::endl ;
+    BOOST_CHECK  ( ( poly[ 0 ] ) ==  array_0 ) ;
+    BOOST_CHECK  ( ( poly[ 1 ] ) ==  array_1 ) ;
+    BOOST_CHECK  ( ( poly[ 2 ] ) ==  array_2 ) ;
+
+    BigNumber fx_0, fx_1, fx_2, eval_at_0, eval_at_1, eval_at_2;
+    fx_0.FromDec( "0" ) ;
+    fx_1.FromDec( "1" ) ;
+    fx_2.FromDec( "2" ) ;
+
+    eval_at_0 = poly( fx_0 ) ;
+    eval_at_1 = poly( fx_1 ) ;
+    eval_at_2 = poly( fx_2 ) ;
+
+    BOOST_TEST  ( eval_at_0.ToDec( ) == "1" ) ;
+    BOOST_TEST  ( eval_at_1.ToDec( ) == "0" ) ;
+    BOOST_TEST  ( eval_at_2.ToDec( ) == "2" ) ;
+
+    std::ostringstream ss ;
+    ss << poly ;
+    BOOST_TEST ( ss.str( ) == "modulo = 5;	polynomial = 1 + 0x + 4x^2" ) ;    
 }
 
 
