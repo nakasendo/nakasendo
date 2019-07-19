5a7776b7b7e7f6116fcb7366311f7c33976930c0
davies <davies@ncrypt.com>
2019-07-19 11:41:53 +0100

    SL-172 Additional Python interface & Confirmation testing on Symmetric Encryption/Decryption (PySymEncDec)

--- tests/functional/test_SymEncoDecode.py
+++ tests/functional/test_SymEncoDecode.py
@@ -2,13 +2,21 @@ import PySymEncDec
 import random
 import string
 
-bitSize = "256"
-
 def randomString(stringLength):
 
     #Generate a random string of fixed length
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
+
+def test_Generate_Nonce():
+
+    for x in range(100):
+
+        # Generate a nonce value of fixed length.
+        nonce_value = PySymEncDec.GenerateNounce()
+
+        # verifying the length of nonce value as 32
+        assert len(nonce_value) == 32, "Test failed"
 
 def test_GenerateKey():
 
@@ -17,16 +25,21 @@ def test_GenerateKey():
         # Generate a random string of fixed length
         String = randomString(10)
 
-        #Generate a key from random string of fixed length
-        actual_value = PySymEncDec.GenerateKey(String, bitSize)
+        # Generate a nonce value of fixed length.
+        nonce_value = PySymEncDec.GenerateNounce()
+
+        #Generate a key from random generated nonce value
+        actual_value = PySymEncDec.GenerateKey(String, nonce_value)
 
         # verifying the actual value with the expected value
         assert len(actual_value) == 64, "Test failed"
 
 def test_EncodeAES(test_data_dir):
 
+    nonce_value = "c280f8553e93485876e423b1d3f2e3a7"
+
     # Generate a key from random string of fixed length
-    genKey = PySymEncDec.GenerateKey("ghmgpipbmz", bitSize)
+    genKey = PySymEncDec.GenerateKey("ghmgpipbmz", nonce_value)
 
     # verifying the generated key
     assert len(genKey) == 64, "Test failed"
@@ -38,14 +51,16 @@ def test_EncodeAES(test_data_dir):
 
             strEncode = x.split(",")
 
-            actual_value = PySymEncDec.encodeAES(strEncode[0], genKey, bitSize)
+            actual_value = PySymEncDec.encodeAES(strEncode[0], genKey, nonce_value)
 
             assert actual_value == strEncode[1].rstrip("\n"), "Test failed"
 
 def test_DecodeAES(test_data_dir):
 
+    nonce_value = "c280f8553e93485876e423b1d3f2e3a7"
+
     # Generate a key from random string of fixed length
-    genKey = PySymEncDec.GenerateKey("ghmgpipbmz", bitSize)
+    genKey = PySymEncDec.GenerateKey("ghmgpipbmz", nonce_value)
 
     # verifying the generated key
     assert len(genKey) == 64, "Test failed"
@@ -57,14 +72,16 @@ def test_DecodeAES(test_data_dir):
 
             strEncode = x.split(",")
 
-            actual_value = PySymEncDec.decodeAES(strEncode[1].rstrip("\n"), genKey, bitSize)
+            actual_value = PySymEncDec.decodeAES(strEncode[1].rstrip("\n"), genKey, nonce_value)
 
             assert actual_value == strEncode[0], "Test failed"
 
 def test_EncodeDecodeAES(test_data_dir):
 
+    nonce_value = "c280f8553e93485876e423b1d3f2e3a7"
+
     # Generate a key from random string of fixed length
-    genKey = PySymEncDec.GenerateKey("ghmgpipbmz", bitSize)
+    genKey = PySymEncDec.GenerateKey("ghmgpipbmz", nonce_value)
 
     # verifying the generated key
     assert len(genKey) == 64, "Test failed"
@@ -76,7 +93,7 @@ def test_EncodeDecodeAES(test_data_dir):
 
             strEncode = x.split(",")
 
-            encode_value = PySymEncDec.encodeAES(strEncode[0], genKey, bitSize)
-            actual_value = PySymEncDec.decodeAES(encode_value, genKey, bitSize)
+            encode_value = PySymEncDec.encodeAES(strEncode[0], genKey, nonce_value)
+            actual_value = PySymEncDec.decodeAES(encode_value, genKey, nonce_value)
 
             assert actual_value == strEncode[0], "Test failed"
