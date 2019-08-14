9d539d792cbd146839ebf9d5b407dc87d3af420c
davies <davies@ncrypt.com>
2019-08-14 11:32:32 +0100

    SL-210 Retest the key splitting functionality

--- tests/functional/test_SecretSplit.py
+++ tests/functional/test_SecretSplit.py
@@ -1,73 +1,13 @@
 import PyAsymKey
+from random import randint
 
-
+maxshares = 100
 privKeyPEMHeader = "BEGIN EC PRIVATE KEY"
+
 
 def test_SetKeyFromPEM():
 
-    for x in range(100):
-
-        # Generate pair of keys in pem format
-        pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
-        assert privKeyPEMHeader in privKey, "Test failed"
-
-        #Sets a key from a PEM format
-        pubValue, priValue = PyAsymKey.SetKeyFromPem(privKey)
-        assert priValue == privKey, "Test failed"
-        assert pubValue == pubKey, "Test failed"
-
-def test_KeySplit():
-
-    threshold = 20
-    maxshares = 100
-    for x in range(100):
-
-        #Generate pair of keys in pem format
-        pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
-        assert privKeyPEMHeader in privKey, "Test failed"
-
-        #Split a private key into a given number of shares
-        splitKeyList = PyAsymKey.SplitKey(privKey, threshold, maxshares)
-        assert len(splitKeyList) == maxshares, "Test failed"
-
-def test_RestoreKey():
-
-    threshold = 20
-    maxshares = 100
-    for x in range(100):
-
-        thresholdList = []
-        privKeyPEMHeader = "BEGIN EC PRIVATE KEY"
-
-        #Generate pair of keys in pem format
-        pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
-        assert privKeyPEMHeader in privKey, "Test failed"
-
-        #Split a private key into a given number of shares
-        splitKeyList = PyAsymKey.SplitKey(privKey, threshold, maxshares)
-        assert len(splitKeyList) == maxshares, "Test failed"
-
-        for i in range (threshold):
-
-            thresholdList.append(splitKeyList[i])
-
-        assert len(thresholdList) == threshold, "Test failed"
-
-        #convert list to String
-        thresholdString = ';'.join(thresholdList)
-
-        #Restore a private key from a given number of shares
-        restorePubKey, restorePrivKey = PyAsymKey.RestoreKey(thresholdString)
-        assert restorePrivKey == privKey, "Test failed"
-
-def test_SetSplitRestoreKey():
-
-    threshold = 20
-    maxshares = 100
-    for x in range(100):
-
-        thresholdList = []
-        privKeyPEMHeader = "BEGIN EC PRIVATE KEY"
+    for x in range(10):
 
         # Generate pair of keys in pem format
         pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
@@ -78,47 +18,111 @@ def test_SetSplitRestoreKey():
         assert priValue == privKey, "Test failed"
         assert pubValue == pubKey, "Test failed"
 
-        #Split a private key into a given number of shares
+
+def test_KeySplit():
+
+    for x in range(10):
+
+        #Randomly generate two digit number that ranges from 3 to 100
+        threshold = randint(3, 100)
+
+        # Generate pair of keys in pem format
+        pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
+        assert privKeyPEMHeader in privKey, "Test failed"
+
+        # Split a private key into a given number of shares
         splitKeyList = PyAsymKey.SplitKey(privKey, threshold, maxshares)
         assert len(splitKeyList) == maxshares, "Test failed"
 
-        for i in range (threshold):
+
+def test_RestoreKey():
+
+    for x in range(10):
+
+        thresholdList = []
+        # Randomly generate two digit number that ranges from 3 to 100
+        threshold = randint(3, 100)
+
+        # Generate pair of keys in pem format
+        pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
+        assert privKeyPEMHeader in privKey, "Test failed"
+
+        # Split a private key into a given number of shares
+        splitKeyList = PyAsymKey.SplitKey(privKey, threshold, maxshares)
+        assert len(splitKeyList) == maxshares, "Test failed"
+
+        for i in range(threshold):
 
             thresholdList.append(splitKeyList[i])
 
         assert len(thresholdList) == threshold, "Test failed"
 
-        #convert list to String
+        # convert list to String
         thresholdString = ';'.join(thresholdList)
 
-        #Restore a private key from a given number of shares
+        # Restore a private key from a given number of shares
         restorePubKey, restorePrivKey = PyAsymKey.RestoreKey(thresholdString)
         assert restorePrivKey == privKey, "Test failed"
 
+
+def test_SetSplitRestoreKey():
+
+    for x in range(10):
+
+        thresholdList = []
+        # Randomly generate two digit number that ranges from 3 to 100
+        threshold = randint(3, 100)
+
+        # Generate pair of keys in pem format
+        pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
+        assert privKeyPEMHeader in privKey, "Test failed"
+
+        # Sets a key from a PEM format
+        pubValue, priValue = PyAsymKey.SetKeyFromPem(privKey)
+        assert priValue == privKey, "Test failed"
+        assert pubValue == pubKey, "Test failed"
+
+        # Split a private key into a given number of shares
+        splitKeyList = PyAsymKey.SplitKey(privKey, threshold, maxshares)
+        assert len(splitKeyList) == maxshares, "Test failed"
+
+        for i in range(threshold):
+
+            thresholdList.append(splitKeyList[i])
+
+        assert len(thresholdList) == threshold, "Test failed"
+
+        # convert list to String
+        thresholdString = ';'.join(thresholdList)
+
+        # Restore a private key from a given number of shares
+        restorePubKey, restorePrivKey = PyAsymKey.RestoreKey(thresholdString)
+        assert restorePrivKey == privKey, "Test failed"
+
+
 def test_ThresholdEqualMaxShareTestcase():
 
-    threshold = 100
-    maxshares = 100
     thresholdList = []
-    privKeyPEMHeader = "BEGIN EC PRIVATE KEY"
+    #Randomly generate two digit number that ranges from 3 to 100
+    threshold = 100
 
-    #Generate pair of keys in pem format
+    # Generate pair of keys in pem format
     pubKey, privKey = PyAsymKey.GenerateKeyPairPEM()
     assert privKeyPEMHeader in privKey, "Test failed"
 
-    #Split a private key into a given number of shares
+    # Split a private key into a given number of shares
     splitKeyList = PyAsymKey.SplitKey(privKey, threshold, maxshares)
     assert len(splitKeyList) == maxshares, "Test failed"
 
-    for i in range (threshold):
+    for i in range(threshold):
 
         thresholdList.append(splitKeyList[i])
 
     assert len(thresholdList) == threshold, "Test failed"
 
-    #convert list to String
+    # convert list to String
     thresholdString = ';'.join(thresholdList)
 
-    #Restore a private key from a given number of shares
+    # Restore a private key from a given number of shares
     restorePubKey, restorePrivKey = PyAsymKey.RestoreKey(thresholdString)
     assert restorePrivKey == privKey, "Test failed"
