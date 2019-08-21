e94a80565c041454f4b58b81c87d3d04b3a1785e
John Murphy <j.murphy@nchain.com>
2019-08-21 13:40:34 +0100

    Fix for broken MessageHash test & Secret Split test on Windows (SL-214)

--- tests/unit/test_MessageHash.cpp
+++ tests/unit/test_MessageHash.cpp
@@ -118,3 +118,4 @@ BOOST_AUTO_TEST_CASE(test_codec_base64_particular_case)// This case has failed o
 /// TODO add a loop : similar test with random inputs : random msg, decode(encode(msg)) should return the message
 BOOST_AUTO_TEST_SUITE_END()
 
+
