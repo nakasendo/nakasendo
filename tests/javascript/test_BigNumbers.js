8fbfc4c92b010bd1d796490172d128db8f69601b
Chi Thanh NGUYEN <c.nguyen@nchain.com>
2019-10-22 09:57:32 +0100

    add test javascript for ctest

--- tests/javascript/test_BigNumbers.js
+++ tests/javascript/test_BigNumbers.js
@@ -1,25 +1,24 @@
-// Find a way to do const Module = global.JsBigNumbers
-const Module = require('JsBigNumbers.js');
-
 var assert = require('assert');
-/*
-describe('Example test suite', function() {
-  describe('Example test case', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-*/
+
+//console.log('Test run in test_BigNumbers.js SDK_MODULE_PATH=' + SDK_MODULE_PATH);
+//console.log('Test run in test_BigNumbers.js SDK_DEBUG_POSTFIX=' + SDK_DEBUG_POSTFIX);
+
+module.paths.push(SDK_MODULE_PATH); //console.log(module.paths);
+var JsModuleName = 'JsBigNumbers'+ SDK_DEBUG_POSTFIX +'.js'
+const JSBigNumbers = require(JsModuleName);
+
 
 describe('Random generator', function() {
   it('Generate random Hex', function() {
-      const rand_hex_str  = Module.RandHex(128);
+      const rand_hex_str  = JSBigNumbers.RandHex(128);
       console.log(rand_hex_str);
   });
   it('Generate random Dex', function() {
-    const rand_dec_str  = Module.RandDec(128);
+    const rand_dec_str  = JSBigNumbers.RandDec(128);
     console.log(rand_dec_str);
   });
 
 });
+
+// Remove the require cache
+delete require.cache[require.resolve(JsModuleName)]
